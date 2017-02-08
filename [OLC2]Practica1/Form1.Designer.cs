84bb3229a74f57fd81c9b4d6a9e71d0d066983fa
kjhg4589 <kjhg.45.89@gmail.com>
2017-02-08 15:23:12 -0600

    Creacion de practica para inicial para compiladores 2

---
+++ [OLC2]Practica1/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace _OLC2_Practica1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.guardarComoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ejecutarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ejecutarToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.reporteErroresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.albunDeImagenesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rc_consola = new System.Windows.Forms.RichTextBox();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem,
+            this.ayudaToolStripMenuItem,
+            this.guardarToolStripMenuItem,
+            this.guardarComoToolStripMenuItem,
+            this.ejecutarToolStripMenuItem,
+            this.ejecutarToolStripMenuItem1,
+            this.reporteErroresToolStripMenuItem,
+            this.albunDeImagenesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1601, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // guardarComoToolStripMenuItem
+            // 
+            this.guardarComoToolStripMenuItem.Image = global::_OLC2_Practica1.Properties.Resources.save;
+            this.guardarComoToolStripMenuItem.Name = "guardarComoToolStripMenuItem";
+            this.guardarComoToolStripMenuItem.Size = new System.Drawing.Size(113, 20);
+            this.guardarComoToolStripMenuItem.Text = "Guardar Como";
+            // 
+            // ejecutarToolStripMenuItem
+            // 
+            this.ejecutarToolStripMenuItem.Name = "ejecutarToolStripMenuItem";
+            this.ejecutarToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.ejecutarToolStripMenuItem.Text = "Cerrar";
+            this.ejecutarToolStripMenuItem.Click += new System.EventHandler(this.ejecutarToolStripMenuItem_Click);
+            // 
+            // ejecutarToolStripMenuItem1
+            // 
+            this.ejecutarToolStripMenuItem1.Name = "ejecutarToolStripMenuItem1";
+            this.ejecutarToolStripMenuItem1.Size = new System.Drawing.Size(61, 20);
+            this.ejecutarToolStripMenuItem1.Text = "Ejecutar";
+            // 
+            // reporteErroresToolStripMenuItem
+            // 
+            this.reporteErroresToolStripMenuItem.Name = "reporteErroresToolStripMenuItem";
+            this.reporteErroresToolStripMenuItem.Size = new System.Drawing.Size(99, 20);
+            this.reporteErroresToolStripMenuItem.Text = "Reporte Errores";
+            // 
+            // albunDeImagenesToolStripMenuItem
+            // 
+            this.albunDeImagenesToolStripMenuItem.Name = "albunDeImagenesToolStripMenuItem";
+            this.albunDeImagenesToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.albunDeImagenesToolStripMenuItem.Text = "Imagenes";
+            this.albunDeImagenesToolStripMenuItem.Click += new System.EventHandler(this.albunDeImagenesToolStripMenuItem_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Location = new System.Drawing.Point(12, 27);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1057, 729);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rc_consola);
+            this.groupBox1.Location = new System.Drawing.Point(1076, 49);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(513, 703);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Consola";
+            // 
+            // rc_consola
+            // 
+            this.rc_consola.BackColor = System.Drawing.SystemColors.Window;
+            this.rc_consola.Enabled = false;
+            this.rc_consola.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rc_consola.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.rc_consola.Location = new System.Drawing.Point(7, 20);
+            this.rc_consola.Name = "rc_consola";
+            this.rc_consola.Size = new System.Drawing.Size(500, 677);
+            this.rc_consola.TabIndex = 0;
+            this.rc_consola.Text = "";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.Image = global::_OLC2_Practica1.Properties.Resources.plus;
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.archivoToolStripMenuItem.Text = "Nuevo";
+            // 
+            // ayudaToolStripMenuItem
+            // 
+            this.ayudaToolStripMenuItem.Image = global::_OLC2_Practica1.Properties.Resources.folder;
+            this.ayudaToolStripMenuItem.Name = "ayudaToolStripMenuItem";
+            this.ayudaToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.ayudaToolStripMenuItem.Text = "Abrir";
+            this.ayudaToolStripMenuItem.Click += new System.EventHandler(this.ayudaToolStripMenuItem_Click);
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Image = global::_OLC2_Practica1.Properties.Resources.save1;
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1601, 768);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "SBScript";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ayudaToolStripMenuItem;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox rc_consola;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarComoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ejecutarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ejecutarToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem reporteErroresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem albunDeImagenesToolStripMenuItem;
+    }
+}
+
---
+++ [OLC2]Practica1/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _OLC2_Practica1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ayudaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ejecutarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void albunDeImagenesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
