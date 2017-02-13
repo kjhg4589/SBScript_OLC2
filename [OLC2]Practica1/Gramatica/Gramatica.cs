646f57f68b83bd77b94f2aec06b60596ffdc855e
kjhg4589 <kjhg.45.89@gmail.com>
2017-02-12 21:36:25 -0600

    Se agrega clase principal para realizar la gramatica

---
+++ [OLC2]Practica1/Gramatica/Gramatica.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _OLC2_Practica1.Gramatica
+{
+    class Gramatica
+    {
+    }
+}
